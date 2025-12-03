d9bf1c0e684455f8c16c772428ac37c0fdf5d311
Andredsonn <andreaesilva6@gmail.com>
2025-12-02 22:19:42 -0300

    script banco

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using ProjetoMidasAPI.Models;
 
 namespace ProjetoMidasAPI.Data
 {
-    // Essa classe é o "coração" do EF Core: conecta os Models ao banco
+    // Essa classe é o "coração" do EF(migração) Core:, pq conecta as Models ao banco
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
@@ -12,17 +12,17 @@ namespace ProjetoMidasAPI.Data
         public DbSet<Projecao> Projecoes { get; set; } = null!;
         public DbSet<Lancamento> Lancamentos { get; set; } = null!;
 
-        // É aqui que você coloca o OnModelCreating
+        // É aqui o OnModelCreating
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Define nomes de tabelas e chaves primárias
+            // Define nomes de tabelas para serem alimentadas e chamadas. Também defini o relacionamentos e pk e fk
             modelBuilder.Entity<Projecao>().ToTable("Projecoes");
             modelBuilder.Entity<Projecao>().HasKey(p => p.IdProjecao);
 
             modelBuilder.Entity<Lancamento>().ToTable("Lancamentos");
             modelBuilder.Entity<Lancamento>().HasKey(l => l.IdLancamento);
 
-            // Corrige os avisos de decimal
+            // decimal
             modelBuilder.Entity<Projecao>()
                 .Property(p => p.ValorPrevisto)
                 .HasColumnType("decimal(18,2)");
